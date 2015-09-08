cca965752ff1963886c1152fc8f4289a3650d99b
Dave Judd <52185dj@gmail.com>
2015-09-08 10:56:30 -0500

    A dynamic, self-updating copyright paragraph in PHP.
    This is a simple 1-line script to keep a web site's copyright paragraph
    up to date by checking the current date on page load and updating the
    year.

---
+++ copyright.php
@@ -0,0 +1,14 @@
+<?php
+
+	$startDate = "2010"; //This is the beginning date of the Copyright date range
+	$siteName = "DaveJudd.com"; //This is the name of your website displaying the copyright
+
+	echo "Copyright &copy; " . $startDate;
+	
+	if (date("Y") > $startDate) {
+		echo "-".date("Y");
+	}
+	
+	echo " All Rights Reserved.";
+	
+?>
